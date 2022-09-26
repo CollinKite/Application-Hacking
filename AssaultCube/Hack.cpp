0216c7b09a36f6d8be4a9a579d7e77a46da7f38e
Collin Kite <me@collinkite.com>
2022-09-26 14:15:27 -0600

    Transitioned to Object Approach With Memory

--- AssaultCube/Hack.cpp
+++ AssaultCube/Hack.cpp
@@ -1,51 +1,64 @@
 #define WIN32_LEAN_AND_MEAN
 #include "PointersAndOffsets.h"
+#include "Memory.h"
 #include <Windows.h>
 #include <TlHelp32.h>
 #include <iostream>
 
 int main()
 {
-	//Handle to a window
-	HWND hWnd = FindWindow(nullptr, "AssaultCube");
-	std::cout << "Window Handle is: " << hWnd << std::endl;
+	Memory mem("AssaultCube");
+
+	std::cout << "Process Id: " << mem.pID << std::endl;
+
+	std::cout << "Module Address: " << mem.GetModuleAddress("ac_client.exe") << std::endl;
+
+
+
+
+
+
 	
-	//Initial Process ID var, will fill when we get the id
-	DWORD pID = NULL; //DWORD is just a unsigned long int
+	////Handle to a window
+	//HWND hWnd = FindWindow(nullptr, "AssaultCube");
+	//std::cout << "Window Handle is: " << hWnd << std::endl;
+	//
+	////Initial Process ID var, will fill when we get the id
+	//DWORD pID = NULL; //DWORD is just a unsigned long int
 
-	//Get Process ID from the Window Handle
-	GetWindowThreadProcessId(hWnd, &pID);
+	////Get Process ID from the Window Handle
+	//GetWindowThreadProcessId(hWnd, &pID);
 
-	std::cout << "Process ID is: " << pID << std::endl;
+	//std::cout << "Process ID is: " << pID << std::endl;
 
-	//Open Handle to Process by pID, with all access. (false to not inherit)
-	HANDLE pHandle = OpenProcess(PROCESS_ALL_ACCESS, false, pID);
+	////Open Handle to Process by pID, with all access. (false to not inherit)
+	//HANDLE pHandle = OpenProcess(PROCESS_ALL_ACCESS, false, pID);
 
-	ac::localEntity* localPlayer = new ac::localEntity();
+	//ac::localEntity* localPlayer = new ac::localEntity();
 
 
-	//ReadProcessMemory(pHandle, (LPVOID)localPlayer->entity_ptr, (LPVOID)localPlayer->entity, sizeof(DWORD), 0);
-	//ReadProcessMemory(pHandle, (LPVOID)ammoReserve_address, &ammoReserve, sizeof(ammo), 0);
-	//std::cout << "local entity address -> " << localPlayer->entity << std::endl;
-	//std::cout << "Current Ammo: " << ammo << std::endl;
-	//std::cout << "Current Ammo Reserve: " << ammoReserve << std::endl;
-	std::cout << "Time for Unlimited Ammo :)" << std::endl;
-	
-	bool quit = false;
-	const int maxAmm = 69;
-	const int maxAmmR = 420;
+	////ReadProcessMemory(pHandle, (LPVOID)localPlayer->entity_ptr, (LPVOID)localPlayer->entity, sizeof(DWORD), 0);
+	////ReadProcessMemory(pHandle, (LPVOID)ammoReserve_address, &ammoReserve, sizeof(ammo), 0);
+	////std::cout << "local entity address -> " << localPlayer->entity << std::endl;
+	////std::cout << "Current Ammo: " << ammo << std::endl;
+	////std::cout << "Current Ammo Reserve: " << ammoReserve << std::endl;
+	//std::cout << "Time for Unlimited Ammo :)" << std::endl;
+	//
+	//bool quit = false;
+	//const int maxAmm = 69;
+	//const int maxAmmR = 420;
 
-	while (!quit)
-	{
-		//WriteProcessMemory(pHandle, (LPVOID)ammo_address, &maxAmm, sizeof(int), 0); //Update Ammo to 69
-		//WriteProcessMemory(pHandle, (LPVOID)ammoReserve_address, &maxAmmR, sizeof(int), 0); //Update Ammo Reserve to 420
-		if (GetKeyState(VK_ESCAPE))
-		{
-			quit = true;
-		}
-	}
+	//while (!quit)
+	//{
+	//	//WriteProcessMemory(pHandle, (LPVOID)ammo_address, &maxAmm, sizeof(int), 0); //Update Ammo to 69
+	//	//WriteProcessMemory(pHandle, (LPVOID)ammoReserve_address, &maxAmmR, sizeof(int), 0); //Update Ammo Reserve to 420
+	//	if (GetKeyState(VK_ESCAPE))
+	//	{
+	//		quit = true;
+	//	}
+	//}
 
-	std::cout << "Cheat Exited" << std::endl;
+	//std::cout << "Cheat Exited" << std::endl;
 
 	
 
---
+++ AssaultCube/Memory.h
@@ -0,0 +1,75 @@
+#pragma once
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#include <TlHelp32.h>
+#include <string>
+
+class Memory
+{
+public:
+	//Constructor that Finds Process id and opens handle 
+	Memory(const std::string& processName)
+	{
+		//Open Handle to Window, with the process name
+		HWND hWnd = FindWindow(nullptr, processName.c_str());
+
+		//Get Process ID from the Window Handle
+		GetWindowThreadProcessId(hWnd, &pID);
+
+		//Open Handle to Process with full access by pID
+		HANDLE pHandle = OpenProcess(PROCESS_ALL_ACCESS, false, pID);
+
+
+	}
+
+	~Memory()
+	{
+		if (pHandle) ::CloseHandle(pHandle);	
+	}
+
+	// Returns the base address of a module by name - Credit: https://github.com/cazzwastaken/external-aimbot/blob/main/memory.h
+	const std::uintptr_t GetModuleAddress(const std::string_view moduleName) const noexcept
+	{
+		::MODULEENTRY32 entry = { };
+		entry.dwSize = sizeof(::MODULEENTRY32);
+
+		const auto snapShot = ::CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, pID);
+
+		std::uintptr_t result = 0;
+
+		while (::Module32Next(snapShot, &entry))
+		{
+			if (!moduleName.compare(entry.szModule))
+			{
+				result = reinterpret_cast<std::uintptr_t>(entry.modBaseAddr);
+				break;
+			}
+		}
+
+		if (snapShot)
+			::CloseHandle(snapShot);
+
+		return result;
+	}
+	
+	//Reads memory from a given address
+	template<typename T>
+	T readMemory(DWORD address)
+	{
+		T buffer;
+		ReadProcessMemory(pHandle, (LPVOID)address, &buffer, sizeof(T), 0);
+		return buffer;
+	}
+
+	//Writes memory to a given address
+	template<typename T>
+	void writeMemory(DWORD address, T value)
+	{
+		WriteProcessMemory(pHandle, (LPVOID)address, &value, sizeof(T), 0);
+	}
+
+public:
+	DWORD pID = 0;
+	void* pHandle = nullptr;
+
+};
